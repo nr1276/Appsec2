0bad8251f93204e8514b41f5aef50dc5e1b14be3
Naresh Ramlogan <nr1276@nyu.edu>
2019-10-16 23:54:35 +0000

    update

--- app.py
+++ app.py
@@ -18,17 +18,17 @@ csrf = CSRFProtect()
 
 
 class RegistrationForm(Form):
-    username = StringField('Username', [validators.Length(min=4, max=25)])
-    password = PasswordField('New Password', [
+    uname = StringField('Username', [validators.Length(min=4, max=25)])
+    pword = PasswordField('New Password', [
             validators.DataRequired(),
             validators.length(min=6, max=20)
         ])
     mfa = StringField('mfa', [validators.DataRequired(), validators.Length(min=10, max=20)])
-    result = StringField('result')
+    success = StringField('result')
 
 class UserLoginForm(Form):
-    username = StringField('Username', [validators.DataRequired()])
-    password = PasswordField('Password', [validators.DataRequired()])
+    uname = StringField('Username', [validators.DataRequired()])
+    pword = PasswordField('Password', [validators.DataRequired()])
     mfa = StringField('mfa', [validators.DataRequired()])
     result = StringField('result')
 
@@ -61,7 +61,7 @@ def user_loader(username):
 
 @login_manager.request_loader
 def request_loader(request):
-    username = request.form.get('username')
+    username = request.form.get('uname')
     if username not in Users:
         return
     user = User()
@@ -80,35 +80,35 @@ def mainpage(user=None):
 def register():
     form = RegistrationForm(request.form)
     if request.method ==  'POST' and form.validate():
-        username = form.username.data
-        password = sha256_crypt.encrypt(form.password.data)
+        uname = form.uname.data
+        pword = sha256_crypt.encrypt(form.pword.data)
         mfa = form.mfa.data
-        if username in Users:
-            form.username.data = 'user already exists'
-            form.result.data = 'failure'
+        if uname in Users:
+            form.uname.data = 'user already exists'
+            form.success.data = 'failure'
             return render_template('register.html', form=form)
-        Users[username] = {'password': password, 'mfa': mfa}
-        form.result.data = 'success'
+        Users[uname] = {'password': pword, 'mfa': mfa}
+        form.success.data = 'success'
     return render_template('register.html', form=form)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = UserLoginForm(request.form)
     if request.method == 'POST':
-       username = form.username.data
-       password = form.password.data
+       uname = form.uname.data
+       pword = form.pword.data
        mfa = form.mfa.data
-       if (username not in Users):
+       if (uname not in Users):
            form.result.data = "incorrect"
            return render_template('login.html', form=form)
-       if (not sha256_crypt.verify(password, Users[username]['password'])):
+       if (not sha256_crypt.verify(pword, Users[uname]['password'])):
            form.result.data = "incorrect"
            return render_template('login.html', form=form)
-       if (mfa != Users[username]['mfa']):
+       if (mfa != Users[uname]['mfa']):
            form.result.data = "Two-factor failure"
            return render_template('login.html', form=form) 
        user = User()
-       user.id = username
+       user.id = uname
        flask_login.login_user(user)
        form.result.data = "success"
     return render_template('login.html', form=form)
