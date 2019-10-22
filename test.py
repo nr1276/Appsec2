6a08d701a4b626d451be5c3648854e5e009160ea
Naresh Ramlogan <nr1276@nyu.edu>
2019-10-22 03:58:26 +0000

    new test

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import unittest
 import requests
+from app import app
 
 server_address="http://127.0.0.1:5000"
 SERVICE_ADDR=server_address
@@ -7,42 +8,55 @@ SERVICE_ADDR=server_address
 
 class FeatureTest(unittest.TestCase):
 
-    def test_server_is_alive(self):
-        req = requests.get(server_address)
-        self.assetEqual(req.status_code, 200)
+    def setUp(self):
+        self.app = app.test_client()
+        app.testing = True
 
+    def test_server_is_alive(self):
+        req = requests.get(server_address + "/register")
+        self.assetrEqual(req.status_code, 200)
+        print("OK")
 
     def test_login(self):
-        req = requests.get(serer_address + "/login")
+        req = requests.get(server_address + "/login")
         self.assertEqual(req.status_code, 200)
+        print("OK")
 
     def test_page_exits(self):
         PAGES = ["/register", "/login", "/spell_check"]
         for page in PAGES:
                 req = requests.get(server_address + page)
                 self.assertEqual(req.status_code, 200)
+                print("OK")
 
     def test_no_login(self):
-        req = requests.get(server_address + "spell_check")
-        self.assetEqual(req.status_code, 400)
+        req = requests.get(server_address + "/spell_check")
+        self.assertEqual(req.status_code, 400)
+        print("OK")
 
     def test_spell_check(self):
-        req = requests.get(server_address + "register")
+        req = requests.get(server_address + "/register")
         self.assertEqual(req.status_code, 200)
         uname = 'tjramlogan'
         pword = "blahblah"
         mfa = "1234567891"
         # Add register code
         # check successful
-
-        req = requests.get(server_address + "login")
-        self.assetEqual(req.status_code, 200)
+        req = requests.get(server_address + "/login")
+        self.assertEqual(req.status_code, 200)
         # login
         # check successful
+        print("OK")
 
-        req = requests.get(server_address + "spell_check")
-        self.assetEqual(req.status_code, 200)
+        req = requests.get(server_address + "/spell_check")
+        self.assertEqual(req.status_code, 200)
 
 
+    def tearDown(self):
+        pass
+
+if __name__ == '__main__':
+    
+    unittest.main()
 
 
